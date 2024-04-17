3e527e463d6c42c9d2ae4297fe3d14d95277500d
wanghao <a00332211@vip.qq.com>
2024-04-17 12:04:57 +0800

    1.删除父pom中modules标签 2.新增CacheStudy模块   2.1.添加redis基础配置   2.2.新增RedisConfig配置类(指定redis的key和value的序列化方式)   2.3.新增RedisUtil类(封装了对redis的key、string、hash、list、set、zset的基本操作,后续可能新增pipeline操作)

---
+++ cache-study/src/main/java/wh/cachestudy/CacheStudy.java
@@ -0,0 +1,11 @@
+package wh.cachestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStudy {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheStudy.class, args);
+    }
+}
